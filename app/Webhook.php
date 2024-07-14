3fd91c79f6803fe6af8b397a5bb7f6c2f9603df5
giyas <gias@m.insta.kim>
2024-07-14 12:49:24 +0600

    ok

--- app/Webhook.php
+++ app/Webhook.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Webhook extends Model
 {
     use HasFactory;
+    protected $table = 'webhooks';
+
+    // Define the attributes that are mass assignable
+    protected $fillable = ['data'];
 }
