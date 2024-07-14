861020b9296c94f9de9367214b9667f10a4c302c
giyas <gias@m.insta.kim>
2024-07-14 11:18:29 +0600

    add login fb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,6 +64,7 @@
         </style>
     </head>
     <body>
+    <button id="fb-login-btn">Login with Facebook</button>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -95,5 +96,61 @@
                 </div>
             </div>
         </div>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '1087087365982506', // Replace with your app ID
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v19.0'  // Replace with the API version you are using
+            });
+
+            FB.AppEvents.logPageView();
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <script>
+        function handleLoginResponse(response) {
+            if (response.authResponse) {
+                console.log('Welcome! Fetching your information....');
+                FB.api('/me', function(response) {
+                    console.log('Good to see you, ' + response.name + '.');
+                    fetchPages(); // Fetch pages after successful login
+                });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        }
+
+        function fetchPages() {
+            FB.api('/me/accounts', function(response) {
+                if (response && !response.error) {
+                    console.log('Pages you manage:', response);
+                    // Automatically select the page or handle the selection logic here
+                    if (response.data.length > 0) {
+                        let page = response.data[0]; // Automatically select the first page
+                        console.log('Automatically selected page:', page);
+                        // Additional logic to handle the selected page
+                    } else {
+                        console.log('No pages found.');
+                    }
+                } else {
+                    console.error('Error fetching pages', response.error);
+                }
+            });
+        }
+
+        document.getElementById('fb-login-btn').addEventListener('click', function() {
+            FB.login(handleLoginResponse, {scope: 'pages_show_list,leads_retrieval, pages_manage_metadata,pages_manage_ads'});
+        });
+    </script>
     </body>
 </html>
